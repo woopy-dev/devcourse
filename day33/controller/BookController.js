39f09320e5292692cf1f909b9481c44603c7aeee
woopy-dev <woopy.dev@gmail.com>
2025-05-16 16:30:12 +0900

    05/16 Day 33

--- day33/controller/BookController.js
+++ day33/controller/BookController.js
@@ -2,8 +2,30 @@ const conn = require('../mariadb.js');
 const { StatusCodes } = require('http-status-codes');
 
 const allBooks = (req, res) => {
-  let sql = `SELECT * FROM books`;
-  conn.query(sql, (err, results) => {
+  let { category_id, news, limit, currentPage } = req.query;
+
+  let offset = limit * (currentPage - 1);
+
+  let sql = "SELECT * FROM books";
+  let values = [];
+
+  if (category_id && news) {
+    // 카테고리별 최신 도서 조회
+    sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 10 YEAR) AND NOW()`;
+    values.push(category_id);
+  } else if (category_id) {
+    // 카테고리별 도서 조회
+    sql += ` WHERE category_id = ?`;
+    values.push(category_id);
+  } else if (news) {
+    // 최신 도서 조회
+    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 10 YEAR) AND NOW()`;
+  }
+
+  sql += " LIMIT ? OFFSET ?";
+  values.push(parseInt(limit), offset);
+
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
@@ -16,7 +38,12 @@ const allBooks = (req, res) => {
 const bookDetail = (req, res) => {
   const { id } = req.params;
 
-  let sql = `SELECT * FROM books WHERE id = ?`;
+  let sql = `
+    SELECT * FROM books 
+    LEFT JOIN category 
+    ON books.category_id = category.id
+    WHERE books.id=?
+  `;
   let values = [id];
 
   conn.query(sql, values, (err, results) => {
