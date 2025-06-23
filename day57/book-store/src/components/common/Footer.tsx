3bdfb4fa3696670613ac9d19475a1c3bf9a50fe1
woopy-dev <woopy.dev@gmail.com>
2025-06-23 17:38:33 +0900

    06/23 Day 57

---
+++ day57/book-store/src/components/common/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <>
+      <hr />
+      <footer>copyright(c), 2025, book store.</footer>
+    </>
+  )
+}
+
+export default Footer;
---
+++ day57/book-store/src/components/common/Sidebar.tsx
@@ -0,0 +1,7 @@
+function Sidebar() {
+  return (
+    <div>sidebar</div>
+  )
+}
+
+export default Sidebar;
